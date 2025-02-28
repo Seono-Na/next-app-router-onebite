4fe1556129a29a76b4504237d11f9e3b5a8b79ab
Seono-Na <seono5670@gmail.com>
2025-02-28 22:30:53 +0900

    refactor: import 정리

--- src/actions/create-review.action.ts
+++ src/actions/create-review.action.ts
@@ -1,9 +1,8 @@
 "use server";
 
-import { error } from "console";
-import { revalidatePath, revalidateTag } from "next/cache";
+import { revalidateTag } from "next/cache";
 
-export async function createReviewAction(_: any, formData: FormData) {
+export async function createReviewAction(_: unknown, formData: FormData) {
   const bookId = formData.get("bookId")?.toString();
   const content = formData.get("content")?.toString();
   const author = formData.get("author")?.toString();
